df173e5a8f3e5d1d59def777f64c67ecf77efe8d
Ülküm Ezgi Akbaş <126782676+ulkumezgiakbas@users.noreply.github.com>
2024-12-10 13:19:15 +0300

    Add files via upload

---
+++ Spotify_Time_Machine_Project/main.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+billboard_url = "https://www.billboard.com/charts/hot-100/" + date
+response = requests.get(url=billboard_url, headers=header)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+
+# Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://localhost:4304/auth/spotify/callback",
+        client_id="709c39819ef341db899a4109838a84c3",
+        client_secret="36bfac3d660b4b9cab7d96d651a2426a",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
